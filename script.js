b9814ec9b5117c109452341b4c5b94a07c005e43
Rohan859 <93860329+Rohan859@users.noreply.github.com>
2024-05-19 01:02:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+// Select all input fields
+const inputs = document.querySelectorAll('.code');
+
+// Loop over each input field
+inputs.forEach((input, index) => {
+    // Add keyup event listener
+    input.addEventListener('keyup', (e) => {
+        // If key entered is a number, focus on next input field
+        if(e.key >= 0 && e.key <= 9) {
+            if(index < inputs.length - 1) {
+                inputs[index + 1].focus();
+            }
+        }
+        // If key is not a number, clear the input field
+        else {
+            input.value = '';
+        }
+    });
+
+    // Add keydown event listener
+    input.addEventListener('keydown', (e) => {
+        // If backspace is pressed, focus on previous input field
+        if(e.key === 'Backspace') {
+            if(index > 0) {
+                inputs[index - 1].focus();
+            }
+        }
+    });
+});
